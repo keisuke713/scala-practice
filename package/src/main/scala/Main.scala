f947f537cc5b47dd65fc35e555a9a4cba6eec97e
keisuke713 <k.nebashi@homie.co.jp>
2026-09-11 09:26:08 +0000

    lesson10

--- package/src/main/scala/Main.scala
+++ package/src/main/scala/Main.scala
@@ -1,10 +1,16 @@
 import my.example.io._
 import my.areas.{Circle => C}
+import my.example._
 
 object Main extends App {
   println("Hello, World!")
-  val e = new MyExample()
-  println(e.readFileIntoString("./src/main/resource/hoge.txt"))
-  val c = new C()
-  println(c)
+
+  val p = new Party()
+  p.register(9)
+  val cost = p.cost()
+  println(s"costs for 10 people: $cost")
+  p.cost()
+
+  val e = new Employee()
+  println(e.name)
 }
---
+++ package/src/main/scala/my/example/Employee.scala
@@ -0,0 +1,7 @@
+package my.example
+
+class Employee {
+    val name: String = "kei"
+    protected val age: Int = 30
+    private val salary: Double = 1000000000
+}
