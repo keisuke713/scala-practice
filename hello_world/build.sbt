b19feb6b8d998b5972027dd5abce6b71e13b7371
keisuke713 <k.nebashi@homie.co.jp>
2026-09-14 01:08:05 +0000

    lesson14

--- hello_world/build.sbt
+++ hello_world/build.sbt
@@ -17,6 +17,8 @@ name := "hello-world"
 organization := "ch.epfl.scala"
 version := "1.0"
 
+scalacOptions += "-Xfatal-warnings"
+
 // Note, it's not required for you to define these three settings. These are
 // mostly only necessary if you intend to publish your library's binaries on a
 // place like Sonatype.
--- hello_world/src/main/scala/Main.scala
+++ hello_world/src/main/scala/Main.scala
@@ -1,6 +1,25 @@
 object Main extends App {
-  println("Hi Kei! Hello, World!")
+    println("Hi Kei! Hello, World!")
+    // val currency: Currency = CAD
+    // val hoge = currency match {
+    //     case USD => 1
+    //     case GBP => 0.744
+    //     case EUR => 0.848
+    // }
+    // println(s"currency: ${hoge}")
 
-  val vm = new VendingMachine()
-  println(vm)
+    val value = 100
+    val price = value match {
+        case n: Int if n < 50 => n
+        case n: Int if n < 100 => n * 0.9
+        case n => n * 0.85
+    }
+    println(s"result: $price")
 }
+
+sealed trait Currency
+
+object USD extends Currency
+object GBP extends Currency
+object EUR extends Currency
+object CAD extends Currency
